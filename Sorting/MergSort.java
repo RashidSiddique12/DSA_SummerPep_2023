d43852993ccd3dc7f18c334a5910e5504cc3d93f
Md Rashid Siddique <94442631+RashidSiddique12@users.noreply.github.com>
2023-06-18 23:21:21 +0530

    Add files via upload

--- Sorting/MergSort.java
+++ Sorting/MergSort.java
@@ -1,39 +1,32 @@
 public class MergSort {
-
     public static void merg(int[] arr, int l, int mid, int h) {
         int n = mid - l + 1;
         int m = h - mid;
 
         int L[] = new int[n];
         int R[] = new int[m];
-
-        for (int i1 = 0; i1 < n; i1++) {
-            L[i1] = arr[l + i1];
+        for (int i = 0; i < n; i++) {
+            L[i] = arr[l + i];
         }
-        for (int j1 = 0; j1 < m; j1++) {
-            R[j1] = arr[mid + 1 + j1];
+        for (int j = 0; j < m; j++) {
+            R[j] = arr[mid + 1 + j];
         }
 
-        // Now last merg L & R In arr
-        int i = 0, j = 0;
-        int k = l;
+        int i = 0, j = 0, k = l;
         while (i < n && j < m) {
             if (L[i] <= R[j]) {
-                arr[k] = L[i++];
+                arr[k++] = L[i++];
             } else {
-                arr[k] = R[j++];
+                arr[k++] = R[j++];
             }
-            k++;
         }
 
         while (i < n) {
             arr[k++] = L[i++];
         }
-
         while (j < m) {
             arr[k++] = R[j++];
         }
-
     }
 
     public static void sort(int[] arr, int l, int h) {
@@ -42,9 +35,9 @@ public class MergSort {
             sort(arr, l, mid);
             sort(arr, mid + 1, h);
 
+            // merg
             merg(arr, l, mid, h);
         }
-
     }
 
     public static void print(int[] arr) {
